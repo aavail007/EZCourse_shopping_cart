c939d4add29eec7ef4761e12d2e18a3280b05947
aavail007 <aavail007@gmail.com>
2020-10-28 23:12:33 +0800

    popup 製作

---
+++ js/shoppingCart.js
@@ -0,0 +1,11 @@
+// 開啟 popup 畫面
+function openPopup() {
+    var element = document.getElementById('popupDiv');
+    element.classList.add("open");
+}
+
+
+function closePopup() {
+    var element = document.getElementById('popupDiv');
+    element.classList.remove("open");
+}

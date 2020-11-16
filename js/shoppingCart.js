7257daf17f08c62696140b9876efa936608cb643
aavail007 <aavail007@gmail.com>
2020-11-17 07:13:23 +0800

    修購物車 popup 高度被撐開問題

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -9,3 +9,9 @@ function closePopup() {
     var element = document.getElementById('popupDiv');
     element.classList.remove("open");
 }
+
+// 前往購物車頁面
+//修購物車 popup 高度被撐開問題 
+function goToShoppingInfo() {
+    location.href = './shoppingInfo.html';
+}
